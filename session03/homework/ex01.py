e6b2a268e18da89d5184085ecbed25c90a232a7f
LienNguyen070799 <nguyenthilien07071999@gmail.com>
2018-07-03 06:52:07 +0700

    cats

--- session03/homework/ex01.py
+++ session03/homework/ex01.py
@@ -3,17 +3,17 @@ loop = True
 while loop:
     choose = input( "Welcome to our shop. What do you want (C,R,U,D) ?")
     
-    if choose == "R" :
+    if choose == "R" or choose == "r" :
         print (" Our items: ", end= "")
         print (*clothes, sep =", ")
        
-    elif choose == "C" :
+    elif choose == "C" or choose == "c" :
         new_item = input (" Enter new item: ")
         clothes.append(new_item)
         print(" Our items: ", end= "")
         print(*clothes, sep =", ")
        
-    elif choose == "U" :
+    elif choose == "U" or choose == "u" :
         update = int(input(" update position: "))
         if update > len(clothes):
             print("invalid update location! Try again!")
@@ -22,7 +22,7 @@ while loop:
             clothes [update - 1] = change
             print(" Our items: ", end ="")
             print(*clothes, sep =", ")
-    elif choose == "D":
+    elif choose == "D" or choose == "d" :
         pos_del = int(input(" Delete position: "))
         if pos_del > len(clothes):
             print("invalid update location! Try again!")
@@ -30,7 +30,7 @@ while loop:
             del(clothes[pos_del - 1])
             print( " Our items: ", end = "")
             print(*clothes, sep = ",")
-    elif choose == "Exit":
+    elif choose == "Exit" or choose == "exit" or choose == "EXIT" :
         print("Thank you! See you again!^-^")
         break
     else :
